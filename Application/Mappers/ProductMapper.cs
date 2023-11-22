3c99440e19c33f135b9978e329fc3ba2a9d24943
ghazaleh79 <ghazalehhh137999@gmail.com>
2023-11-22 17:26:25 +0330

    Add mappers

---
+++ Application/Mappers/ProductMapper.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Dtos;
+using Domain.Models;
+
+namespace Application.Mappers;
+
+public static class ProductMapper
+{
+    public static ProductInfo ToProductInfo(this Product product) => new (product.Id, product.Name, product.ProduceDate,
+        product.ManufacturePhone, product.ManufactureEmail, product.IsAvailable, product.User!.ToUserInfo());
+    
+    public static ProductDto ToProductDto(this Product product) => new (product.Id, product.Name, product.ProduceDate,
+        product.ManufacturePhone, product.ManufactureEmail, product.IsAvailable);
+}
---
+++ Application/Mappers/UserMapper.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Dtos;
+using Domain.Models;
+
+namespace Application.Mappers;
+
+public static class UserMapper
+{
+    public static UserInfo ToUserInfo(this User user) => new (user.Id, user.Email, user.Username,
+        user.Products?.Select(product => product.ToProductDto()).ToList());
+}
